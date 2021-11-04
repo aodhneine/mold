03687dfbaf5f91769126fc8f525f7e74cd397c60
Aodhnait Étain <aodhneine@pm.me>
2021-11-04 05:59:52 -0700

    Add build script for limine bootloader
    It automatically handles cloning the latest version of the limine
    bootloader and building the UEFI version of it.
    
    Signed-off-by: Aodhnait Étain <aodhneine@pm.me>

---
+++ limine/build.fish
@@ -0,0 +1,17 @@
+#!/usr/bin/env fish
+
+function info
+  printf '%s=>%s ' (set_color -o blue) (set_color normal)
+  printf '%s%s%s\n' (set_color -o) $argv (set_color normal)
+end
+
+info "cloning limine source code"
+git clone https://github.com/limine-bootloader/limine --depth 1 limine-source
+pushd limine-source
+
+# For some reason I can't build limine using clang anymore.
+info "building limine-uefi"
+make limine-uefi -j17
+
+info "done"
+popd
